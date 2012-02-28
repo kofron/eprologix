fc0dbb7265a85fd03415038fca4cbb42635c7b7f
Jared Kofron <jared.kofron@gmail.com>
2012-02-28 11:18:29 -0800

    added send_command/1 API function that ignores the response entirely by skipping the receiving state of the FSM.  verified to work with agilent 34970A.

--- src/eprologix_cmdr.erl
+++ src/eprologix_cmdr.erl
@@ -2,12 +2,12 @@
 -behavior(gen_fsm).
 
 -record(state,{ip_addr, port, c_sock, c_req}).
--record(request,{from,data,tag}).
+-record(request,{type,from,data,tag}).
 
 %% API
 -export([start_link/0]).
--export([configuring/2,waiting/2,waiting/3,sending/2,receiving/2,parse_reply/2]).
--export([send_query/1]).
+-export([configuring/2,waiting/2,waiting/3,sending/2,receiving/2,parse_reply/2,finishing/2]).
+-export([send_query/1, send_command/1]).
 
 %% gen_fsm callbacks
 -export([init/1, handle_event/3, handle_sync_event/4, handle_info/3, 
@@ -17,7 +17,21 @@
 %%% API %%%
 %%%%%%%%%%%
 send_query(QueryString) ->
-	Req = #request{from = self(), data = QueryString, tag = make_ref()},
+	Req = #request{
+		type = q,
+		from = self(), 
+		data = QueryString, 
+		tag = make_ref()
+	},
+	gen_fsm:send_all_state_event(?MODULE,Req).
+
+send_command(CommandString) ->
+	Req = #request{
+		type = c,
+		from = self(),
+		data = CommandString,
+		tag = make_ref()
+	},
 	gen_fsm:send_all_state_event(?MODULE,Req).
 
 %%%%%%%%%%%%%%%
@@ -50,8 +64,9 @@ handle_sync_event(Event, _From, StateName, StateData) ->
 handle_info({tcp,S,Data}=R, receiving, #state{c_sock=S}=StateData) ->
 	gen_fsm:send_event(?MODULE,R),
 	{next_state, receiving, StateData};
-handle_info(Info, StateName, StateData) ->
-	io:format("(~p)~n",[Info]),
+handle_info({tcp_closed,S}, StateName, #state{c_sock=S}=StateData) ->
+	{stop, socket_abort, StateData};
+handle_info({tcp_closed,_}, StateName, StateData) ->
 	{next_state, StateName, StateData}.
 
 terminate(_Reason, _StateName, _StateData) ->
@@ -82,8 +97,14 @@ sending(timeout, #state{c_req=R,c_sock=S}=StateData) ->
 		false ->
 			[R#request.data|"\n"]
 		end,
+	NextState = case should_wait_for_reply(R) of
+		true ->
+			receiving;
+		false ->
+			finishing
+		end,
 	gen_tcp:send(S,ToSend),
-	{next_state, receiving, StateData,0}.
+	{next_state, NextState, StateData,0}.
 
 receiving({tcp,S,Data}, #state{c_sock=S}=StateData) ->
 	io:format("got reply:~p~n",[Data]),
@@ -92,7 +113,10 @@ receiving(timeout, #state{c_sock=S}=StateData) ->
 	gen_tcp:send(S,"++read eoi\n"),
 	{next_state, receiving, StateData}.
 
-parse_reply(timeout, #state{c_sock=S}=StateData) ->
+parse_reply(timeout, StateData) ->
+	{next_state, finishing, StateData, 0}.
+
+finishing(timeout, #state{c_sock=S}=StateData) ->
 	close_telnet_sock(S),
 	io:format("completed loop.  waiting again...~n"),
 	NewStateData = StateData#state{c_sock = none, c_req = none},
@@ -127,3 +151,8 @@ is_newline_terminated(String) ->
 
 default_conf_string() ->
 	"++clr\n++mode 1\n++auto 0\n++eos 0\n++eoi 1\n".
+
+should_wait_for_reply(#request{type = c}) ->
+	false;
+should_wait_for_reply(#request{type = q}) ->
+	true.
